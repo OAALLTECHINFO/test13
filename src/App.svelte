27b9542264b05402ee998e3450b5107d9f1b21fa
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-23 14:51:59 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { ComboBox } from "fluent-svelte";
+</script>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
+<h2>
+	Hello Guys
+</h2>
+<h2>
+	Welcom to The OA
+</h2>
+<main>
+  hello
+  <ComboBox
+		scroll-behavior: auto
+    value="6"
+    items={Array.from(Array(50).keys()).map((i) => ({
+      name: `Item ${i}`,
+      value: i.toString(),
+    }))} />
+</main>
